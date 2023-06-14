5b2ae3fc2f4806571b16b6b9a02ffafff4ebd9c9
진호인 <hoin.jin@supercat.co.kr>
2023-06-14 16:49:38 +0900

    set api add post method

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -10,5 +10,16 @@ export default function handler(
     req: NextApiRequest,
     res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ text: '센터라벨', centerLabel: '센터 라벨일에요' })
+  if (
+      req.method === 'post'
+  ){
+    const customData = req.body.customData ? ':::: 커스텀데이터(' + req.body.customData + ')' : ''
+    const text = `채팅 메세지 입니다${customData}`
+    const centerLabel = `센터 라벨 입니다${customData}`
+
+    res.status(200).json({ text, centerLabel })
+    return;
+  }
+
+  res.status(200).json({ text: '센터라벨', centerLabel: '센터 라벨 입니다' })
 }
